e377feea58299e51954dc64de95a84d5feff83f1
ahmadmed7at77 <ahmadmed7at77@gmail.com>
2023-02-03 17:21:21 +0200

    build: add eslint & use it precommits via husky

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: "eslint:recommended",
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    "no-unused-vars": [
+      "error",
+      { args: "after-used", argsIgnorePattern: "^_" },
+    ],
+  },
+};
