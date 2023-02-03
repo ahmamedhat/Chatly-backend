42f5164b11a7776efda115fe58be96600a8d1bac
ahmadmed7at77 <ahmadmed7at77@gmail.com>
2023-02-03 17:05:04 +0200

    feat: add mongoose & connect to db ✨

---
+++ src/services/apollo.js
@@ -0,0 +1,29 @@
+const { ApolloServer } = require("apollo-server-express");
+const { loadFilesSync } = require("@graphql-tools/load-files");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
+const path = require("path");
+const app = require("../app");
+
+const typesArray = loadFilesSync(path.join(__dirname, "../../**/*.graphql"));
+const resolversArray = loadFilesSync(
+  path.join(__dirname, "../../**/*.resolvers.js")
+);
+
+const schema = makeExecutableSchema({
+  typeDefs: [typesArray],
+  resolvers: resolversArray,
+});
+
+async function startApolloServer() {
+  const server = new ApolloServer({
+    schema,
+  });
+
+  await server.start();
+
+  server.applyMiddleware({ app, path: "/data" });
+}
+
+module.exports = {
+  startApolloServer,
+};
---
+++ src/services/mongodb.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const mongodbURL = process.env.MONGODB_URL;
+const mongo = mongoose.connection;
+
+async function connectToMongodb() {
+  try {
+    mongoose.set("strictQuery", false);
+    mongoose.connect(mongodbURL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  } catch (error) {
+    console.log("Error connecting to mongodb", error);
+  }
+}
+
+mongo.on("error", console.error.bind(console, "Mongodb connection error: "));
+
+mongo.once("open", function () {
+  console.log("Mongodb Connected successfully");
+});
+
+async function closeMongodbConnection() {
+  await mongoose.disconnect();
+}
+
+module.exports = {
+  connectToMongodb,
+  closeMongodbConnection,
+};
