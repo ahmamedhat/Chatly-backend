822702bc36d1a5988ff5c322df46460449e5e204
ahmadmed7at77 <ahmadmed7at77@gmail.com>
2023-02-17 22:56:55 +0200

    fix: try #5

--- src/services/apollo.js
+++ src/services/apollo.js
@@ -1,18 +1,34 @@
 const { ApolloServer } = require("@apollo/server");
 const { expressMiddleware } = require("@apollo/server/express4");
-const { loadFilesSync } = require("@graphql-tools/load-files");
+// const { loadFilesSync } = require("@graphql-tools/load-files");
 const { makeExecutableSchema } = require("@graphql-tools/schema");
 const path = require("path");
 const { app } = require("../app");
+const { loadFilesSync } = require("@graphql-tools/load-files");
 
 const typesArray = loadFilesSync(path.join(__dirname, "../../**/*.graphql"));
 const resolversArray = loadFilesSync(
   path.join(__dirname, "../../**/*.resolvers.js")
 );
 
+const typeDefs = [
+  /* GraphQL */ `
+    schema {
+      query: RootQuery
+    }
+
+    type RootQuery {
+      aNumber: Int
+    }
+  `,
+];
+
+console.log("types are", typesArray[0].definitions);
+
 const schema = makeExecutableSchema({
-  typeDefs: typesArray,
-  resolvers: resolversArray,
+  // typeDefs: [typesArray],
+  typeDefs,
+  // resolvers: [resolversArray],
 });
 
 async function startApolloServer() {
