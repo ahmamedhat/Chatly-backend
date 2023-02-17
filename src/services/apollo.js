e11eb938988dfbbf4b0c1b5158eae8764a2ce18e
ahmadmed7at77 <ahmadmed7at77@gmail.com>
2023-02-18 01:24:01 +0200

    fix: try #8

--- src/services/apollo.js
+++ src/services/apollo.js
@@ -1,39 +1,47 @@
 const { ApolloServer } = require("@apollo/server");
 const { expressMiddleware } = require("@apollo/server/express4");
 // const { loadFilesSync } = require("@graphql-tools/load-files");
-const { makeExecutableSchema } = require("@graphql-tools/schema");
-// const path = require("path");
+// const { makeExecutableSchema } = require("@graphql-tools/schema");
+const path = require("path");
 const { app } = require("../app");
-// const { loadFilesSync } = require("@graphql-tools/load-files");
+const { loadFilesSync } = require("@graphql-tools/load-files");
+// const { gql } = require("apollo-server");
+const chatsTypeDef = require("../../models/chats/schema");
 
-// const typesArray = loadFilesSync(path.join(__dirname, "../../**/*.graphql"));
-// const resolversArray = loadFilesSync(
-//   path.join(__dirname, "../../**/*.resolvers.js")
-// );
+const typesArray = loadFilesSync(path.join(__dirname, "../../**/*.graphql"));
+const resolversArray = loadFilesSync(
+  path.join(__dirname, "../../**/*.resolvers.js")
+);
+const { chatsResolver } = require("../../models/chats/chats.resolvers");
+const {
+  messagesResolver,
+} = require("../../models/messages/messages.resolvers");
+const usersResolver = require("../../models/users/users.resolvers");
 
-const typeDefs = [
-  /* GraphQL */ `
-    schema {
-      query: RootQuery
-    }
+// console.log("types are ", typesArray);
+// console.log("resolvers are ", resolversArray);
 
-    type RootQuery {
-      aNumber: Int
-    }
-  `,
-];
+const typeDefs = [chatsTypeDef];
+
+const resolvers = [chatsResolver, messagesResolver, usersResolver];
+
+console.log("chats resolver is", chatsResolver);
 
 // console.log("types are", typesArray[0].definitions);
 
-const schema = makeExecutableSchema({
-  // typeDefs: [typesArray],
-  typeDefs,
-  // resolvers: [resolversArray],
-});
+// const schema = makeExecutableSchema({
+//   // typeDefs: [typesArray],
+//   typeDefs: [typesArray],
+//   // resolvers: [resolversArray],
+// });
 
 async function startApolloServer() {
   const server = new ApolloServer({
-    schema,
+    // schema
+    // typeDefs: [typesArray],
+    typeDefs,
+    resolvers,
+    // resolvers: resolversArray,
   });
 
   await server.start();
