862a339f0e7a25e22eb92aa39ca33c4a89a78c3d
ahmadmed7at77 <ahmadmed7at77@gmail.com>
2023-02-21 01:08:20 +0200

    refactor: add graphql schema file and return active users on sockets connect and disconnect

--- src/services/apollo.js
+++ src/services/apollo.js
@@ -1,47 +1,21 @@
 const { ApolloServer } = require("@apollo/server");
 const { expressMiddleware } = require("@apollo/server/express4");
-// const { loadFilesSync } = require("@graphql-tools/load-files");
-// const { makeExecutableSchema } = require("@graphql-tools/schema");
-const path = require("path");
 const { app } = require("../app");
-const { loadFilesSync } = require("@graphql-tools/load-files");
-// const { gql } = require("apollo-server");
-const chatsTypeDef = require("../../models/chats/schema");
-
-const typesArray = loadFilesSync(path.join(__dirname, "../../**/*.graphql"));
-const resolversArray = loadFilesSync(
-  path.join(__dirname, "../../**/*.resolvers.js")
-);
+const chatsTypeDef = require("../../models/schema");
 const { chatsResolver } = require("../../models/chats/chats.resolvers");
 const {
   messagesResolver,
 } = require("../../models/messages/messages.resolvers");
 const usersResolver = require("../../models/users/users.resolvers");
 
-// console.log("types are ", typesArray);
-// console.log("resolvers are ", resolversArray);
-
 const typeDefs = [chatsTypeDef];
 
 const resolvers = [chatsResolver, messagesResolver, usersResolver];
 
-console.log("chats resolver is", chatsResolver);
-
-// console.log("types are", typesArray[0].definitions);
-
-// const schema = makeExecutableSchema({
-//   // typeDefs: [typesArray],
-//   typeDefs: [typesArray],
-//   // resolvers: [resolversArray],
-// });
-
 async function startApolloServer() {
   const server = new ApolloServer({
-    // schema
-    // typeDefs: [typesArray],
     typeDefs,
     resolvers,
-    // resolvers: resolversArray,
   });
 
   await server.start();
--- src/services/socket.js
+++ src/services/socket.js
@@ -2,6 +2,19 @@ const { instrument } = require("@socket.io/admin-ui");
 const { Server } = require("socket.io");
 const { httpServer } = require("../app");
 
+function getActiveUsers(io) {
+  const users = [];
+  for (let [id, socket] of io.of("/").sockets) {
+    users.push({
+      userID: id,
+      username: socket.username,
+      email: socket.email,
+      image: socket.image,
+    });
+  }
+  return users;
+}
+
 async function connectSocketIo() {
   const io = new Server(httpServer, {
     cors: {
@@ -15,27 +28,27 @@ async function connectSocketIo() {
 
   io.use((socket, next) => {
     const username = socket.handshake.auth.username;
-    if (!username) {
+    const email = socket.handshake.auth.email;
+    const image = socket.handshake.auth.image;
+    if (!username || !email) {
       return next(new Error("invalid username"));
     }
     socket.username = username;
+    socket.email = email;
+    socket.image = image;
     next();
   });
 
   io.on("connection", (socket) => {
     console.log("a user connected", socket.id);
-    const users = [];
-    for (let [id, socket] of io.of("/").sockets) {
-      users.push({
-        userID: id,
-        username: socket.username,
-      });
-    }
+    const users = getActiveUsers(io);
+    socket.broadcast.emit("users", users);
     socket.emit("users", users);
     socket.broadcast.emit("new user connected", {
       userID: socket.id,
       username: socket.username,
     });
+
     socket.on("send-message", (message, room) => {
       if (!room) {
         socket.broadcast.emit("receive-message", message);
@@ -45,8 +58,12 @@ async function connectSocketIo() {
       console.log("a message from ", message.sender);
       console.log("saying: ", message.message);
     });
+    socket.on("disconnect", () => {
+      const users = getActiveUsers(io);
+      socket.broadcast.emit("users", users);
+      console.log("user disconnected ", socket.username + socket.id);
+    });
   });
-
   instrument(io, {
     auth: false,
   });
