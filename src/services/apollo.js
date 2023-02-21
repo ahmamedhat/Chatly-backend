ea08e969d666f20b736915f0a67556c07b8481eb
ahmadmed7at77 <ahmadmed7at77@gmail.com>
2023-02-21 04:39:00 +0200

    fix: resolve cors error try #2

--- src/services/apollo.js
+++ src/services/apollo.js
@@ -20,6 +20,23 @@ async function startApolloServer() {
 
   await server.start();
 
+  app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader(
+      "Access-Control-Allow-Origin",
+      "https://chatlylive.vercel.app/"
+    );
+    // Request methods you wish to allow
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, DELETE");
+    // Request headers you wish to allow
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,content-type"
+    );
+    // Pass to next layer of middleware
+    next();
+  });
+
   app.use("/graphql", expressMiddleware(server));
 
   return server;
