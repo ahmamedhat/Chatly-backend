ada0d6718f482bbfa7700c9edfa1468f37a5bdd4
ahmadmed7at77 <ahmadmed7at77@gmail.com>
2023-02-27 07:06:17 +0200

    feat: use database user id in socket auth object instead of default socket id

--- src/services/socket.js
+++ src/services/socket.js
@@ -3,14 +3,16 @@ const { httpServer } = require("../app");
 
 function getActiveUsers(io) {
   const users = [];
-  for (let [id, socket] of io.of("/").sockets) {
+  for (let [socket] of io.of("/").sockets) {
     users.push({
-      userID: id,
+      userID: socket.userID,
       username: socket.username,
       email: socket.email,
       image: socket.image,
     });
   }
+
+  // console.log("all users", users);
   return users;
 }
 
@@ -20,15 +22,19 @@ async function connectSocketIo() {
       origin: ["https://chatlylive.vercel.app", "http://localhost:3000"],
       methods: ["GET", "POST"],
     },
+    //W'll try this fix
+    transports: ["websocket", "polling"],
   });
 
   io.use((socket, next) => {
+    const userID = socket.handshake.auth.userID;
     const username = socket.handshake.auth.username;
     const email = socket.handshake.auth.email;
     const image = socket.handshake.auth.image;
     if (!username || !email) {
       return next(new Error("invalid username"));
     }
+    socket.userID = userID;
     socket.username = username;
     socket.email = email;
     socket.image = image;
@@ -36,28 +42,32 @@ async function connectSocketIo() {
   });
 
   io.on("connection", (socket) => {
-    console.log("a user connected", socket.id);
+    console.log("a user connected", socket.username + " " + socket.userID);
+    socket.join(socket.userID);
     const users = getActiveUsers(io);
     socket.broadcast.emit("users", users);
     socket.emit("users", users);
     socket.broadcast.emit("new user connected", {
-      userID: socket.id,
+      userID: socket.userID,
       username: socket.username,
     });
 
-    socket.on("send-message", (message, room) => {
+    socket.on("send_message", (message, room) => {
+      console.log(message);
+      console.log("room", room);
       if (!room) {
-        socket.broadcast.emit("receive-message", message);
+        socket.broadcast.emit("receive_message", message);
       } else {
-        socket.to(room).emit("receive-message", message);
+        socket.to(room).emit("receive_message", message);
       }
-      console.log("a message from ", message.sender);
+      console.log("user: ", socket.username);
+      console.log("id: ", socket.userID);
       console.log("saying: ", message.message);
     });
     socket.on("disconnect", () => {
       const users = getActiveUsers(io);
       socket.broadcast.emit("users", users);
-      console.log("user disconnected ", socket.username + socket.id);
+      console.log("user disconnected ", socket.username + " " + socket.userID);
     });
   });
 }
