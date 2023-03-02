04d83dd625302eba2dee220466b3f318bfd65d2d
ahmadmed7at77 <ahmadmed7at77@gmail.com>
2023-03-02 03:40:55 +0200

    style: prettify file

--- codgen.ts
+++ codgen.ts
@@ -1,14 +1,18 @@
-
-import type { CodegenConfig } from '@graphql-codegen/cli';
+import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
   overwrite: true,
   schema: "models/schema.ts",
   generates: {
     "src/generated/graphql.ts": {
-      plugins: ["typescript", "typescript-resolvers", "typescript-mongodb", "typescript-document-nodes"]
-    }
-  }
+      plugins: [
+        "typescript",
+        "typescript-resolvers",
+        "typescript-mongodb",
+        "typescript-document-nodes",
+      ],
+    },
+  },
 };
 
 export default config;
